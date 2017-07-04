c6eafb67090fef4ed774b6f9fd9aba1cb95ac26a
Anthony-Kim <anthonykim1985@gmail.com>
2017-07-04 15:44:08 +0900

    [MODIFY] header

--- sh/hdfs-parts-merger.sh
+++ sh/hdfs-parts-merger.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-#USAGE: sh hdfs-parts-merger.sh [hdfs-location]
+#USAGE: sh hdfs-parts-merger.sh [hdfs-location] [header]
 
 hdfsLocation=$1
+header=$2
 dbTableName=`echo ${hdfsLocation} | cut -d'/' -f5`
 dataFileName=`echo ${hdfsLocation} | cut -d'/' -f5 | cut -d'.' -f2`
 dirName=/home/hadoop/hyuk0628/health-care-service/extracted_dataset
@@ -14,11 +15,11 @@ fi
 hdfs dfs -getmerge ${hdfsLocation}/* ${dirName}/${dataFileName}.csv
 
 # Extract the Data Set Header
-ssh -lhadoop nn1 "hive -e \"set hive.cli.print.header=false; show columns in ${dbTableName};\"" > ${dirName}/header
+# ssh -lhadoop nn1 "hive -e \"set hive.cli.print.header=false; show columns in ${dbTableName};\"" > ${dirName}/header
 
 # Insert header to the Data Set
-sed -i ':a;N;$!ba;s/\n/,/g' ${dirName}/header # replace newline character to comman(,)
-header=`cat ${dirName}/header | sed -r 's/\s+//g'`
+# sed -i ':a;N;$!ba;s/\n/,/g' ${dirName}/header # replace newline character to comma(,)
+# header=`cat ${dirName}/header | sed -r 's/\s+//g'`
 echo ${header}
 sed -i -e '1i'${header}'\' ${dirName}/${dataFileName}.csv
 
